8abff371718c8e31df44586d2638c6f152efc616
@JOMS <passwd@ciencias.unam.mx>
2019-12-03 00:14:31 -0600

    Added initial unit test

---
+++ test/unitTest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class utest_mutation-finder(unittest.TestCase):
+    """docstring for utest_mutation-finder."""
+    def __init__(self, arg):
+        super(utest_mutation-finder, self).__init__()
+        self.arg = arg
+
+    def setUp(self):
+        print("SetUp")
+
+if __name__ == '__main__':
+    unittest.main()
